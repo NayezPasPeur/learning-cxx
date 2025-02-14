301452aa51c0a49405e3f2e1ffb21cf9b6c59960
NayezPasPeur <rhm141246718@gmail.com>
2025-02-14 10:18:30 +0800

    fix ex12

--- exercises/12_method_const/main.cpp
+++ exercises/12_method_const/main.cpp
@@ -7,10 +7,11 @@ struct Fibonacci {
     // TODO: 修改方法签名和实现，使测试通过
     // 使用数组引用传递参数，避免指针退化
     // 这里初始化的时候为什么返回的是constexpr ？？ 
-    constexpr Fibonacci(const int (&arr)[11]) {
+    constexpr Fibonacci(const int (&arr)[11]) : numbers{} {
         for (int i = 0; i < 11; ++i) {
             numbers[i] = arr[i];
         }
+
     }
 
     // `get` 方法应该是 `constexpr`，并且 `const` 以支持 `constexpr Fibonacci`
